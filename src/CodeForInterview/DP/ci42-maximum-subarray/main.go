d824c5f95fc03650f5a81478a1348e67992c2cee
oluoluo <hustly123@gmail.com>
2021-05-08 10:07:37 +0800

    完成最大连续子数组和

---
+++ src/CodeForInterview/DP/ci42-maximum-subarray/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// 连续子数组的最大和：思想很简单，dp[i]表示以i作为最后一个元素的连续子数组的和，最后取最大值即可
+func maxSubArray(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	for i := 1; i < len(dp); i++ {
+		if dp[i-1] < 0 {
+			dp[i] = nums[i]
+		} else {
+			dp[i] = nums[i] + dp[i-1]
+		}
+	}
+	max := math.MinInt32
+	for i := 0; i < len(dp); i++ {
+		if dp[i] > max {
+			max = dp[i]
+		}
+	}
+	return max
+}
+
+func main() {
+	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+}
