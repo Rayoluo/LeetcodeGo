00dabc4a8bf89807b5213d4c3b7723a5936e83b5
oluoluo <hustly123@gmail.com>
2021-05-17 16:13:03 +0800

    完成正则表达式匹配&字符串全排列

--- src/CodeForInterview/Search/ci38-string-permutation/main.go
+++ src/CodeForInterview/Search/ci38-string-permutation/main.go
@@ -1,10 +1,55 @@
 package main
 
-// 字符串的全排列
+import "fmt"
+
+/**
+ * @Author: luo
+ * @Author: hustly123@gmail.com
+ * @Date: 2021/5/17 15:08
+ * @Desc: 给定一个字符串，打印出该字符串中字符的所有排列
+ */
+
+// 思路：用一个map统计每个字符在字符串中出现了多少次, 回溯，剪枝
+var m map[byte]int
+
 func permutation(s string) []string {
-	var
+	p := []byte(s)
+	m = make(map[byte]int)
+	// 统计字符串每个字符出现的次数
+	for i := 0; i < len(p); i++ {
+		if v, ok := m[p[i]]; ok {
+			m[p[i]] = v + 1
+		} else {
+			m[p[i]] = 1
+		}
+	}
+	res := make([]byte, len(s))
+	// set := make(map[string]struct{})
+	// backtrace(res, 0, len(s), set)
+	ss := make([]string, 0)
+	// for k := range set {
+	// 	ss = append(ss, k)
+	// }
+	backtrace(res, 0, len(s), &ss)
+	return ss
+}
+
+func backtrace(res []byte, i int, n int, s *[]string) {
+	if i == n { // 访问完毕
+		str := string(res)
+		*s = append(*s, str)
+		return
+	}
+	for k, v := range m {
+		if v > 0 {
+			res[i] = k
+			m[k] = v - 1
+			backtrace(res, i+1, n, s)
+			m[k] = v
+		}
+	}
 }
 
 func main() {
-
+	fmt.Println(permutation("abc"))
 }
--- src/LeetCode/Search/lc08-regular-expression-match/main.go
+++ src/LeetCode/Search/lc08-regular-expression-match/main.go
@@ -5,32 +5,82 @@ import "fmt"
 /**
  * @Author: luo
  * @Author: hustly123@gmail.com
- * @Date: 2021/5/16 16:09
- * @Desc: 正则表达式匹配
+ * @Date: 2021/5/17 14:21
+ * @Desc: 动态规划之正则表达式
  */
-
-func isMatch(s string, p string) bool {
-	// return backtrack(s, p, 0, 0)
+type pair struct {
+	i, j int
 }
 
-func backtrace(s, p string, l1, l2 int) bool {
-	if p[l2] != '*' { // 当前模式串为普通字符或者.通配符
-		if l2+1 >= len(p) || p[l2+1] != '*' { // 模式串下一个字符不是*
-			if p[l2] == '.' || p[l2] == s[l1] {
-				return backtrace(s, p, l1+1, l2+1)
-			}
-			return false
+var memo map[pair]bool
+
+func isMatch(s string, p string) bool {
+	memo = make(map[pair]bool)
+	return backtrace(s, 0, p, 0)
+}
+
+func backtrace(s string, i int, p string, j int) bool {
+	if v, ok := memo[pair{i, j}]; ok {
+		return v
+	}
+	slen, plen := len(s), len(p)
+	var res bool
+	// base case1: 模式串**匹配完了**，如果字符串刚好访问完则返回true，否则返回false
+	if j == plen {
+		res = i == slen
+		memo[pair{i, j}] = res
+		return res
+	}
+	// base case2: 字符串访问完了，则普通字符（或者.）和*必须成对出现，没有**的写法
+	if i == slen {
+		if (plen-j)%2 == 1 {
+			res = false
+			// return false
+			memo[pair{i, j}] = res
+			return res
 		}
-		// 模式串下一个字符为*
-		// -- 如果当前模式串字符为普通字符
+		for ; j < plen-1; j += 2 {
+			if p[j+1] != '*' {
+				res = false
+				// return false
+				memo[pair{i, j}] = res
+				return res
+			}
+		}
+		res = true
+		// return true
+		memo[pair{i, j}] = res
+		return res
 	}
 
+	// 当前访问的字符匹配上, 即字符相同或者模式串字符为.
+	if s[i] == p[j] || p[j] == '.' {
+		if j+1 < plen && p[j+1] == '*' {
+			res = backtrace(s, i+1, p, j) || backtrace(s, i, p, j+2)
+			// return backtrace(s, i+1, p, j) || backtrace(s, i, p, j+2)
+			memo[pair{i, j}] = res
+			return res
+		} else {
+			res = backtrace(s, i+1, p, j+1)
+			// return backtrace(s, i+1, p, j+1)
+			memo[pair{i, j}] = res
+			return res
+		}
+	} else { // 当前访问的字符不匹配
+		if j+1 < plen && p[j+1] == '*' {
+			res = backtrace(s, i, p, j+2)
+			// return backtrace(s, i, p, j+2)
+			memo[pair{i, j}] = res
+			return res
+		} else {
+			res = false
+			// return false
+			memo[pair{i, j}] = res
+			return res
+		}
+	}
 }
 
 func main() {
 	fmt.Println(isMatch("aab", "c*a*b"))
-	fmt.Println(isMatch("mississippi", "mis*is*p*."))
-	fmt.Println(isMatch("a", "ab*"))
-	fmt.Println(isMatch("aad", ".*")) // 预期结果为true
-	fmt.Println(isMatch("bbbba", ".*a*a"))
 }
